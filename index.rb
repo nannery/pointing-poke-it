da266226652238bc7124f37e36538414db67f611
David Nanry <dnanry>
2018-04-25 18:08:02 -0500

    try: jira http initial

--- index.rb
+++ index.rb
@@ -2,6 +2,8 @@
 
 require 'cuba'
 require 'securerandom'
+require 'net/http'
+require 'json'
 
 Cuba.define do
   on get do
@@ -11,6 +13,19 @@ Cuba.define do
 
     on '/style.css' do
       File.open('style.css', 'r') { |f| res.write f.read }
+    end
+
+    on 'jira', param('id') do |id|
+      jira_request = Net::HTTP::Get.new('/')
+      jira_request.body = body unless body.nil?
+      request.basic_auth(ENV['JIRA_USERNAME'], ENV['JIRA_PASSWORD'])
+
+      jira_conn             = Net::HTTP.new('https://jira.skintertainment.com')
+      jira_conn.use_ssl     = 'TLSv1'
+      jira_conn.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+      jira_response = jira_conn.request(jira_request)
+      json = JSON.parse(jira_response)
     end
 
     on ':id' do |_id|
