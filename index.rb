e152734ee348ed0db1a40a527f8cc226abd166ca
David Nanry <dnanry>
2018-04-25 07:12:46 -0500

    chore: remove commented previous code

--- index.rb
+++ index.rb
@@ -6,8 +6,6 @@ require 'securerandom'
 Cuba.define do
   on get do
     on ':id' do |_id|
-      # res.write 'hi bro'
-      # render 'index.html'
       File.open('index.html', 'r') do |file|
         res.write file.read
       end
